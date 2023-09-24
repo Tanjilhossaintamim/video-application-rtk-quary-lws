bfb901f7222c61cca6635220cb7b31f5b79f8713
Tanjil Hossain <tanjilhossain845.com>
2023-09-24 19:35:59 +0600

    compleate show video funtionality using rtk query

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,15 +1,28 @@
+import { useGetVideosQuery } from "../../redux/features/api/apiSlice";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
+import Error from "../ui/Error";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+  // decisan what will render
+  let content = null;
+  if (isLoading)
+    content = (
+      <>
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+      </>
     );
+  if (!isLoading && isError)
+    content = <Error message={"An Error Occoured !"} />;
+  if (!isError && !isLoading && videos?.length == 0)
+    content = <Error message={"No Video Found !"} />;
+  if (!isError && !isLoading && videos?.length > 0) {
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  }
+  return content;
 }
